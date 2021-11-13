e52a841dbac2186b06f6594ad8067eb8f5ee6d3b
Alfred Obialo <alfredcsdinc@gmail.com>
2021-11-13 13:27:29 +0100

    Add TodoService.ts make http Calls to backend Todo Api Call

---
+++ src/app/modules/feature-modules/todoApp/services/TodoService.ts
@@ -0,0 +1,23 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {BaseHttpService} from "../../../BaseHttpService";
+import {ITodoItem} from "../model/ITodoItem";
+import {Observable} from "rxjs";
+
+
+@Injectable({providedIn: "root"})
+export class TodoService extends BaseHttpService {
+  baseUrl : string = `${this.apiEndPoint}/todo`;
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  getTodos() : Observable<ITodoItem[]> {
+
+    return this.http.get<ITodoItem[]>(this.baseUrl);
+  }
+  addTodo(todoItem : ITodoItem) : Observable<any>{
+    // add a todo to list of Todos on the server
+    return this.http.post(this.baseUrl,todoItem);
+  }
+}
